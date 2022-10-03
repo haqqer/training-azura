9886605f4334a87f711ca0e43a1ad3becba6c4af
Muhammad Ilham Haqqi <ilhamserbaguna@gmail.com>
2022-10-03 20:01:46 +0700

    add docker-compose

--- README.md
+++ README.md
@@ -3,5 +3,21 @@ Training Container and Virtualization
 
 Isi folder
 
-1. ```docker-node``` tutorial deploy node application dengan docker
-2. ```docker-html``` tutorial deploy html dengan docker dan nginx 
+1. ```docker-node``` tutorial deploy node application dengan docker, jalankan command berikut :
+    ```
+    cd docker-node/
+    docker build -t node-app .
+    docker run -it --name node-app -p 3000:3000 node-app
+    ```
+2. ```docker-html``` tutorial deploy html dengan docker dan nginx, jalankan command berikut :
+    ```
+    cd docker-html/
+    docker build -t web-app .
+    docker run -it --name web-app -p 8080:80 web-app
+    ```
+2. ```docker-php``` tutorial penggunaaan docker-compose, jalankan command berikut :
+    ```
+    cd docker-php/
+    docker-compose -f docker-compose.dev.yaml up -d
+    ```
+
---
+++ docker-php/service/app/Http/Controllers/JalanController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Jalan;
+use Illuminate\Http\Request;
+
+class JalanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Jalan  $jalan
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Jalan $jalan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Jalan  $jalan
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Jalan $jalan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Jalan  $jalan
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Jalan $jalan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Jalan  $jalan
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Jalan $jalan)
+    {
+        //
+    }
+}
